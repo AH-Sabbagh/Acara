0b5bfe86e06b2a41d817139988b84a980a02c9dc
ahmadSabbagh <engahamd.sabag1998@gmail.com>
2021-10-20 21:16:00 +0300

    Add Seed Data and Config Migration

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,5 +1,6 @@
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace Infrastructure.Data
 {
@@ -10,11 +11,19 @@ namespace Infrastructure.Data
         {
         }
         public DbSet<Product> Products { get; set; }
-
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> productTypes { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
             optionsBuilder.UseSqlite("Data Source=acara.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder _modelBuilder)
+        {
+            base.OnModelCreating(_modelBuilder);
+            _modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); 
+
         }
 
     }
