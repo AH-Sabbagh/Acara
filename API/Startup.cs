59a510e22ac12449b1c543af88627ded66838c09
ahmadSabbagh <engahamd.sabag1998@gmail.com>
2021-12-29 13:32:37 +0200

    Add NavBar and Add Models(Product,Pagination)

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,13 +40,6 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            // if (env.IsDevelopment())
-            // {
-            //     app.UseDeveloperExceptionPage();
-            //     app.UseSwagger();
-            //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            // }
-
             app.UseMiddleware<ExceptionMiddleware>();
             app.UseStatusCodePagesWithReExecute("/errors/{0 }");
             app.UseHttpsRedirection();
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Acara';
+  products: IProduct[] | undefined;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get<IPagination>("https://localhost:5001/api/Products/products?pageSize=50").subscribe(
+      (response: IPagination) => {
+        this.products = response.data;
+        console.log(response);
+      }, error => { console.log(error); });
+  }
 }
